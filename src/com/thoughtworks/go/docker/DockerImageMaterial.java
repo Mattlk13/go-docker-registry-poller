e3855eae9bb1174e7031cd0994dedfe082b8ab9d
Derek Hammer <derek.r.hammer@gmail.com>
2014-11-18 16:00:22 -0800

    Target 1.7, not 1.8 and add Extension

--- src/com/thoughtworks/go/docker/DockerImageMaterial.java
+++ src/com/thoughtworks/go/docker/DockerImageMaterial.java
@@ -1,9 +1,11 @@
 package com.thoughtworks.go.docker;
 
+import com.thoughtworks.go.plugin.api.annotation.Extension;
 import com.thoughtworks.go.plugin.api.material.packagerepository.PackageMaterialConfiguration;
 import com.thoughtworks.go.plugin.api.material.packagerepository.PackageMaterialPoller;
 import com.thoughtworks.go.plugin.api.material.packagerepository.PackageMaterialProvider;
 
+@Extension
 public class DockerImageMaterial implements PackageMaterialProvider {
     @Override
     public PackageMaterialConfiguration getConfig() {
